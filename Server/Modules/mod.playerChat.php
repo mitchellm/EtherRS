90c3faf41979204d11c49abb41014be86009f5c8
David Harris <lolidunno@live.co.uk>
2013-01-19 17:28:41 -0800

    Update Server/Modules/mod.playerChat.php

--- Server/Modules/mod.playerChat.php
+++ Server/Modules/mod.playerChat.php
@@ -3,9 +3,15 @@ namespace Server\Modules;
 
 class playerChat extends \Server\Server {
 	private $server;
-	
+
 	public function __construct(\Server\Server $server) {
 		$this->log('Player Chat loaded');
+	}
+
+	public function __onLogin(array $args) {
+		$player = $args[1];
+		$ip = $player->getIP();
+		$this->log($player->getUsername() . ' has logged in from ' . $ip['ip'] . ':' . $ip['port']);
 	}
 }
 ?>

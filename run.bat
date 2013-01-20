ddc3d0801841a44a8b15d365e80522d5ca4435ff
David Harris <lolidunno@live.co.uk>
2013-01-19 16:04:10 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
-#REM Change this to your PHP directory
+REM Change this to your PHP directory
 cd C:\path\to\php
 
-#REM Change this to your run script.
+REM Change this to your run script.
 php C:\path\to\EtherRS\run.php
 pause

441853deea90df67d870ac2a2d3e5f7cab521d93
David Harris <lolidunno@live.co.uk>
2013-01-20 16:28:54 -0800

    Update Server/config.Server.php

--- Server/config.Server.php
+++ Server/config.Server.php
@@ -4,3 +4,8 @@ define('SERVER_LISTEN', '127.0.0.1');
 define('SERVER_NAME', 'EtherRS');
 define('DEBUG_CONFIG', false);
 define('CYCLE_TIME', 600);
+
+define('SQL_HOST', 'localhost');
+define('SQL_DB', 'EtherRS');
+define('SQL_USER', 'root');
+define('SQL_PASS', '');
